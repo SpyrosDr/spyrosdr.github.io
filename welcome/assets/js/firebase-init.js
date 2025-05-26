d05bd998a0d437de2b8b1a9efcd6400b28cbeaa3
SpyrosDr <120277325+SpyrosDr@users.noreply.github.com>
2025-05-26 19:59:48 +0200

    Update firebase-init.js

--- welcome/assets/js/firebase-init.js
+++ welcome/assets/js/firebase-init.js
@@ -1,13 +1,14 @@
 // Firebase SDK should already be loaded before this file runs
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "your-project.firebaseapp.com",
-  databaseURL: "https://your-project.firebaseio.com",
-  projectId: "your-project",
-  storageBucket: "your-project.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "XXXX",
+  apiKey: "AIzaSyAtABHYMrBLich7vGedXQ4F3ahBgv07eDU",
+  authDomain: "dragonson-46c50.firebaseapp.com",
+  databaseURL: "https://dragonson-46c50-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "dragonson-46c50",
+  storageBucket: "dragonson-46c50.firebasestorage.app",
+  messagingSenderId: "595136654471",
+  appId: "1:595136654471:web:54a4fe587c730fa3bd9c22",
+  measurementId: "G-0NV8EQ508S",
 };
 
 // Initialize Firebase (only once)
