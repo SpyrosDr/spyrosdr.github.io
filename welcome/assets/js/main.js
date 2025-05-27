c70a3d2c9bafd3ed0744ede52ae5bc411bc11902
SpyrosDr <120277325+SpyrosDr@users.noreply.github.com>
2025-05-27 17:13:38 +0200

    Update main.js

--- welcome/assets/js/main.js
+++ welcome/assets/js/main.js
@@ -397,5 +397,9 @@
 					$window.on('load', function() {
 						$main._show(location.hash.substr(1), true);
 					});
+// Contact actions
+document.getElementById('call-button')?.setAttribute('href', `tel:${data.phone}`);
+document.getElementById('email-button')?.setAttribute('href', `mailto:${data.email}`);
+
 
 })(jQuery);
